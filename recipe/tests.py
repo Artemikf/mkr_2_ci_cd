cdc0e5e68c85b140390015518f40d00e9a134668
Artem Kurta <artemdog386@gmail.com>
2026-05-22 14:20:41 +0300

    feat: create tests

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,113 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Recipe, Category
 
-# Create your tests here.
+
+class MainViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name='Desserts')
+        # Create 7 recipes to verify only 5 are shown
+        for i in range(1, 8):
+            Recipe.objects.create(
+                title=f'Recipe {i}',
+                description=f'Description {i}',
+                instructions=f'Instructions {i}',
+                ingredients=f'Ingredients {i}',
+                category=self.category,
+            )
+
+    def test_main_view_status_code(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_view_uses_correct_template(self):
+        response = self.client.get(reverse('main'))
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_view_returns_only_5_recipes(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(len(response.context['recipes']), 5)
+
+    def test_main_view_returns_latest_recipes(self):
+        response = self.client.get(reverse('main'))
+        recipes = list(response.context['recipes'])
+        # Latest recipes should be returned first (ordered by -created_at)
+        for i in range(len(recipes) - 1):
+            self.assertGreaterEqual(
+                recipes[i].created_at, recipes[i + 1].created_at
+            )
+
+    def test_main_view_no_recipes(self):
+        Recipe.objects.all().delete()
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['recipes']), 0)
+
+    def test_main_view_recipes_have_required_fields(self):
+        response = self.client.get(reverse('main'))
+        recipe = response.context['recipes'][0]
+        self.assertTrue(hasattr(recipe, 'title'))
+        self.assertTrue(hasattr(recipe, 'description'))
+        self.assertTrue(hasattr(recipe, 'ingredients'))
+        self.assertTrue(hasattr(recipe, 'instructions'))
+        self.assertTrue(hasattr(recipe, 'created_at'))
+        self.assertTrue(hasattr(recipe, 'updated_at'))
+
+
+class CategoryListViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.cat1 = Category.objects.create(name='Soups')
+        self.cat2 = Category.objects.create(name='Salads')
+        Recipe.objects.create(
+            title='Borscht',
+            description='Ukrainian soup',
+            instructions='Cook it',
+            ingredients='Beets, cabbage',
+            category=self.cat1,
+        )
+        Recipe.objects.create(
+            title='Green Soup',
+            description='Light soup',
+            instructions='Blend it',
+            ingredients='Spinach, broth',
+            category=self.cat1,
+        )
+        Recipe.objects.create(
+            title='Caesar',
+            description='Classic salad',
+            instructions='Toss it',
+            ingredients='Romaine, croutons',
+            category=self.cat2,
+        )
+
+    def test_category_list_view_status_code(self):
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_list_view_uses_correct_template(self):
+        response = self.client.get(reverse('category_list'))
+        self.assertTemplateUsed(response, 'category_list.html')
+
+    def test_category_list_view_returns_all_categories(self):
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(len(response.context['categories']), 2)
+
+    def test_category_list_view_has_recipe_count(self):
+        response = self.client.get(reverse('category_list'))
+        categories = {c.name: c for c in response.context['categories']}
+        self.assertEqual(categories['Soups'].recipe_count, 2)
+        self.assertEqual(categories['Salads'].recipe_count, 1)
+
+    def test_category_list_view_empty(self):
+        Category.objects.all().delete()
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['categories']), 0)
+
+    def test_category_list_view_category_has_name(self):
+        response = self.client.get(reverse('category_list'))
+        for category in response.context['categories']:
+            self.assertTrue(hasattr(category, 'name'))
+            self.assertIsNotNone(category.name)
